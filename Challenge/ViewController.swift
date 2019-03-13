81309dfcda42f35e6d60101f62c2496a28ddf898
Teck Hian Wee <teckhianwee@dexian.local>
2019-03-13 13:15:50 +0800

    Initial Commit

---
+++ Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge
+//
+//  Created by Teck Hian Wee on 13/3/19.
+//  Copyright © 2019 Teck Hian Wee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
